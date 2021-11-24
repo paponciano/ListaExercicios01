25214aec3b3282b60707144ff3e30b9b88281ab8
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-11-24 11:59:36 -0300

    Commit do Exercício 03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor de produto: ");
+            double valor01 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite agora o segundo valor de produto: ");
+            double valor02 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro valor de produto: ");
+            double valor03 = Convert.ToDouble(Console.ReadLine());
+
+            if (valor01 < valor02 && valor01 < valor03)
+            {
+                Console.WriteLine("O Produto 01 deve ser comprado");
+            }
+            else if (valor02 < valor03)
+            {
+                Console.WriteLine("O Produto 02 deve ser comprado");
+            }
+            else
+            {
+                Console.WriteLine("O Produto 03 deve ser comprado");
+            }
+        }
+    }
+}
