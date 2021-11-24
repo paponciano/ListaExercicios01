18383840c56f7120a0e50df9e095290227e0f334
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-11-24 15:47:04 -0300

    Commit do exercício 06

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio06
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console
+                .WriteLine("Digite um número que corresponde ao dia da semana. (1 a 7)");
+            int diaDaSemana = Convert.ToInt32(Console.ReadLine());
+
+            switch (diaDaSemana)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda-feira");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça-feira");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta-feira");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta-feira");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta-feira");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                    Console.WriteLine("Dia da semana inválido");
+                    break;
+            }
+        }
+    }
+}
